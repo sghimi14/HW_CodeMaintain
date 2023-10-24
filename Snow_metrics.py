7ec9237eaf7dd315ce84a96b1e1c0e79f738e346
Swastik Ghimire <sghimi14@asu.edu>
2023-10-24 15:55:15 -0700

    Code maintained version

--- Snow_metrics.py
+++ Snow_metrics.py
@@ -1,3 +1,4 @@
+#IMPORTING ALL THE LIBRARIES AND MODULES
 import geopandas as gpd
 import pandas as pd
 import numpy as np
@@ -7,23 +8,31 @@ import matplotlib.pyplot as plt
 warnings.filterwarnings('ignore')
 import os
 import seaborn as sns
-
-crb = gpd.read_file('/Users/sghimi14/Documents/Research/Analysis_Basin_Shapefiles/basin_CRB_poly.shp')
-crb = crb.to_crs('epsg:4326')
-
-lower = gpd.read_file('/Users/sghimi14/Documents/Research/Analysis_Basin_Shapefiles/basin_LowerBasin_poly.shp')
-lower = lower.to_crs('epsg:4326')
-
-upper = gpd.read_file('/Users/sghimi14/Documents/Research/Analysis_Basin_Shapefiles/basin_UpperBasin_poly.shp')
-upper = upper.to_crs('epsg:4326')
-
 import glob
+from shapely.geometry import Poin
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+
+#IMPORTING SHAPEFILES OF CRB, UPPER BASIN,LOWER BASIN AND CHANGING PROJECTION
+crb_shp = gpd.read_file('/Users/sghimi14/Documents/Research/Analysis_Basin_Shapefiles/basin_CRB_poly.shp')
+crb = crb_shp.to_crs('epsg:4326')
+
+lower_shp = gpd.read_file('/Users/sghimi14/Documents/Research/Analysis_Basin_Shapefiles/basin_LowerBasin_poly.shp')
+lower = lower_shp.to_crs('epsg:4326')
+
+upper_shp = gpd.read_file('/Users/sghimi14/Documents/Research/Analysis_Basin_Shapefiles/basin_UpperBasin_poly.shp')
+upper = upper_shp.to_crs('epsg:4326')
+
+
+#LOCATING AND COMBINING ALL NETCDF FILES
 file_paths_2018_2023 = glob.glob('Outputs_2018_2023/*.nc')
 file_paths_2010_2017 = glob.glob('Outputs_2010_2017/*.nc')
 file_paths_2004_2009 = glob.glob('Outputs_2004_2009/*.nc')
 
 all_file_paths = file_paths_2018_2023 + file_paths_2010_2017 + file_paths_2004_2009
 
+
+#ANALYSIS
 snodas_data = xr.open_mfdataset(all_file_paths)
 
 SWE = snodas_data.SWE
@@ -39,6 +48,8 @@ new_time_values = new_time_values[~new_time_values.isin(missing_dates)]
 snodas_clipped = SWE.rio.clip(crb.geometry)
 snodas_clipped['Time'] = new_time_values
 
+
+#METADATA AND SITE EXTRACTION
 metadata = pd.read_csv('snotel_metadata.csv')
 snotel_data = pd.read_csv('snotel_pivoted.csv', index_col=0, parse_dates=True)
 
@@ -50,11 +61,9 @@ req_sites = req_sites[['site_id', 'latitude', 'longitude', 'elev']]
 req_sites = req_sites.set_index('site_id').sort_index()
 sites_dict = req_sites.to_dict(orient='index')
 
-from shapely.geometry import Point
-
-for each in sites_dict:
-    station_name = each
-    station_info = sites_dict[each]
+for each_st in sites_dict:
+    station_name = each_st
+    station_info = sites_dict[each_st]
 
     lat = station_info['latitude']
     lon = station_info['longitude']
@@ -67,14 +76,10 @@ for each in sites_dict:
 
     SWE_df.to_csv(f'SNODAS_ts/{station_name}.csv')
 
-import os
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-import numpy as np
-
 snotel_data = pd.read_csv('snotel_pivoted.csv', index_col=0, parse_dates=True)
 
+
+#PLOT CREATION
 files = os.listdir('SNODAS_ts')
 files = [file for file in files if file.endswith('.csv')
 
@@ -100,7 +105,7 @@ for file in files:
     mae = mean_absolute_error(merged_data['SNOTEL'], merged_data['SNODAS'])
     bias = np.mean(merged_data['SNODAS'] - merged_data['SNOTEL'])
 
-    name = int(name)
+    name = int(name) #This will help extract the columns from the dataframe
     elevation = sites_dict[name]['elev']
 
     elevation_values.append(elevation)
@@ -109,6 +114,7 @@ for file in files:
     mae_values.append(mae)
     bias_values.append(bias)
 
+#Storing values in a dataframe
 corr_df = pd.DataFrame({'Elevation': elevation_values, 'Correlation': corr_values})
 rmse_df = pd.DataFrame({'Elevation': elevation_values, 'RMSE': rmse_values})
 mae_df = pd.DataFrame({'Elevation': elevation_values, 'MAE': mae_values})
@@ -116,23 +122,27 @@ bias_df = pd.DataFrame({'Elevation': elevation_values, 'Bias': bias_values})
 
 fig, axs = plt.subplots(2, 2, figsize=(8, 8), sharey=True)
 
+#Correlation Plot
 axs[0, 0].scatter(corr_df['Correlation'], corr_df['Elevation'], alpha=0.7, s=10)
 axs[0, 0].set_xlabel('Correlation', fontweight='bold')
 axs[0, 0].set_ylabel('Elevation (m)', fontweight='bold')
 axs[0, 0].set_title('Elevation control on Correlation', fontweight='bold')
 axs[0, 0].grid(True)
 
+#RMSE Plot
 axs[0, 1].scatter(rmse_df['RMSE'], rmse_df['Elevation'], alpha=0.7, s=10)
 axs[0, 1].set_xlabel('RMSE', fontweight='bold')
 axs[0, 1].set_title('Elevation control on RMSE', fontweight='bold')
 axs[0, 1].grid(True)
 
+#MAE Plot
 axs[1, 0].scatter(mae_df['MAE'], mae_df['Elevation'], alpha=0.7, s=10)
 axs[1, 0].set_xlabel('MAE', fontweight='bold')
 axs[1, 0].set_ylabel('Elevation (m)', fontweight='bold')
 axs[1, 0].set_title('Elevation control on MAE', fontweight='bold')
 axs[1, 0].grid(True)
 
+#Bias plot
 axs[1, 1].scatter(bias_df['Bias'], bias_df['Elevation'], alpha=0.7, s=10)
 axs[1, 1].set_xlabel('Percentage Bias', fontweight='bold')
 axs[1, 1].set_title('Elevation control on Percentage Bias', fontweight='bold')
